7a19c6c1702c162c844a365d34fe2537e2b450c6
BootCamp123 <wajid21@hotmail.co.uk>
2022-10-06 21:34:38 +0100

    completed without read me

---
+++ script.js
@@ -0,0 +1,10 @@
+let names = ["hasan","ali","shah","kenny","ben"]
+console.log(names.length)
+
+let result = []
+
+for (let i =0; i < names.length; i++){
+    result.push( "Great to see you," +names[i]) 
+}
+
+console.log(result)
